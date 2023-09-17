727371ac41ac39225e347f1969767acf96a14a80
Joao <joaoprodutor92@gmail.com>
2023-09-17 16:12:17 -0300

    cria componente form cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 
 import Header from "./components/Header";
+import FormularioNovoMedicamento from "./components/FormularioNovoMedicamento";
 
 function App() {
 
   return (
     <>
       <Header />
+      <FormularioNovoMedicamento />
     </>
   )
 }
