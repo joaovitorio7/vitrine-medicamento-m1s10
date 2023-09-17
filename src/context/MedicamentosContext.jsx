82bcde765284676b455fe0a12016ef5726fbfcfe
Joao <joaoprodutor92@gmail.com>
2023-09-17 16:51:29 -0300

    função add medicamento

--- src/context/MedicamentosContext.jsx
+++ src/context/MedicamentosContext.jsx
@@ -6,6 +6,20 @@ export const MedicamentoContextProvider = ({children}) => {
 
   const [listaMedicamentos, setListaMedicamentos] = useState([])
 
+  const AdicionarMedicamento = (nome, laboratorio, preco) => {
+    const novoMedicamento = {
+      id: listaMedicamentos.length + 1,
+      nome: nome,
+      laboratorio: laboratorio,
+      preco: preco,
+      favorito: false
+    } 
+
+    const novaLista = [...listaMedicamentos]
+    //novaLista.push(novoMedicamento)
+    setListaMedicamentos(novaLista)
+  } 
+
   return(
     <MedicamentoContext.Provider value={{listaMedicamentos, setListaMedicamentos}}>
       {children}
