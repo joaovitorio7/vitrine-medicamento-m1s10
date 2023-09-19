b46b34913aa8860d460d554a9019dee9b1f10dfd
Joao <joaoprodutor92@gmail.com>
2023-09-19 20:18:51 -0300

    cria e chama funcao fav medicamento

--- src/context/MedicamentosContext.jsx
+++ src/context/MedicamentosContext.jsx
@@ -27,8 +27,12 @@ export const MedicamentosContextProvider = ({children}) => {
     alert("Medicamento cadastrado com sucesso!")
   } 
 
+  const FavoritarMedicamento = (id) => {
+
+  }
+
   return(
-    <MedicamentosContext.Provider value={{listaMedicamentos, AdicionarMedicamento}}>
+    <MedicamentosContext.Provider value={{listaMedicamentos, AdicionarMedicamento, FavoritarMedicamento}}>
       {children}
     </MedicamentosContext.Provider>
   )
